60b1b98cd69761bd83c81ce31edd0a0a7b6bbfa4
Federico Cirett <fd.cirett@gmail.com>
2024-12-07 01:06:03 -0700

    Corrected color scheme for graphs
    Made all the graph schemes to 'viridis' to have a uniform visualization of the data colorwise

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -6,6 +6,9 @@ import altair as alt
 import plotly.express as px
 import geopandas as gpd
 import folium
+import pydeck as pdk
+import matplotlib.pyplot as plt
+
 
 #######################
 # Page configuration
@@ -69,15 +72,48 @@ st.markdown("""
 
 #######################
 
+shapefile_path = "data/raw/DATOS GEOREFERENCIALES/2023/2023_1_00_ENT.shp"  # Replace with your shapefile path
+gdf = gpd.read_file(shapefile_path)
+
+# Ensure the GeoDataFrame has latitude and longitude columns for centroids
+gdf['lon'] = gdf.geometry.centroid.x
+gdf['lat'] = gdf.geometry.centroid.y
+gdf['CVE_ENT']=gdf['CVE_ENT'].astype('int')
+
+# Initialize session state to store the selected CVE_ENT
+if "selected_cve_ent" not in st.session_state:
+    st.session_state.selected_cve_ent = None
+
 # Load data
 @st.cache_data
 def load_data(data_file):
     df=pd.read_csv(f"data/processed/ENIGH/{data_file}.csv")
     return df
 
+
 population=load_data('population')
+nivel_nombre_order = [
+    'Ninguno',
+    'Preescolar',
+    'Primaria',
+    'Secundaria', 
+    'Preparatoria o bachillerato',
+    'Profesional',
+    'Normal',
+    'Carrera técnica o comercial',
+    'Maestría',
+    'Doctorado'
+]
+
+# Convert 'nivel_nombre' column to a categorical type with the defined order
+population['nivel_nombre'] = pd.Categorical(population['nivel_nombre'], categories=nivel_nombre_order, ordered=True)
+
+
 income=load_data('income')
 df=pd.merge(income,population, on=['folioviv','foliohog','numren','year','entidad'], how='inner')
+
+background_color = "#1E1E1E"  # Dark gray
+text_color = "#FFFFFF"  # White
 
 
 ####################### Sidebar
@@ -85,7 +121,10 @@ with st.sidebar:
     st.title('🏂 US Population Dashboard')
     
     year_list = list(df.year.unique())[::-1]
+    entidades=list(population.nombre_entidad.unique())[::-1]
+    entidades.append('Nacional')
     
+    selected_entidad = st.selectbox('Selecciona una entidad', entidades)
     selected_year = st.selectbox('Select a year', year_list)
     df_selected_year = df[df.year == selected_year]
     df_selected_year_sorted=df_selected_year.groupby(['year','generacion'],as_index=False)['ing_tri'].mean().sort_values(by="ing_tri", ascending=False).round(2)
@@ -95,29 +134,200 @@ with st.sidebar:
 
 #######################
 # Plots
-
-
-def make_barplot(dataframe,entidad):
     
 
-    df=dataframe[['folioviv','foliohog','sexo','numren','edad','generacion',
-                    'entidad','ing_tri','alfabetism','year']]
-    df[df['entidad']==entidad]
+def plot_literacy_proportion_over_time(population, selected_entidad,text_color,background_color):
+    # Filter the data for the specific 'entidad'
+    df = population[['folioviv', 'foliohog', 'numren', 'alfabetism', 'year', 'entidad', 'sexo', 'nombre_entidad']]
+    is_entidad = df['nombre_entidad'] == selected_entidad
+    if selected_entidad!='Nacional':
+        df = df[is_entidad]
+        
+    
+    df = df.groupby('year', as_index=False)['alfabetism'].value_counts(normalize=True)
+    
+    
+    # Filter for both alfabetism levels: 1 and 2
+    df_alfabetism_1 = df[df['alfabetism'] == 1]
     
 
-    data=df.groupby(['year','generacion'],as_index=False)['ing_tri'].mean()
-    pivot_df = data.pivot(index='year', columns='generacion', values='ing_tri')
+    # Plotting
+    fig, ax = plt.subplots(figsize=(10, 6))
 
-    # Plot
-    pivot_df.plot(kind='bar', figsize=(12, 6), colormap='coolwarm', width=0.8)
+    # Plot for alfabetism == 1
+    ax.plot(
+        df_alfabetism_1['year'],
+        df_alfabetism_1['proportion'],
+        marker='o',
+        label='Alfabetas',
+        color='white'
+    )
+
+    
+    fig.patch.set_facecolor(background_color)  # Figure background
+    ax.set_facecolor(background_color)
+
+    # Customize the plot
+    ax.set_title(f"Proportion of Alfabetism Over Time in {selected_entidad}",color=text_color, fontsize=14)
+    ax.set_xlabel("Year", fontsize=12,color=text_color)
+    ax.set_ylabel("Proportion", fontsize=12,color=text_color)
+    ax.set_xticks(df['year'].unique())
+    ax.tick_params(colors=text_color)
+    ax.legend(title="Alfabetism Level")
+    ax.grid(True, linestyle='--', alpha=0.6)
+
+    
+
+    # Show the plot in the Streamlit app
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+def plot_proportion_by_year(population,selected_entidad,text_color,background_color):
+    # Select necessary columns
+    df = population[['folioviv', 'foliohog', 'numren', 'nivelaprob', 'year', 'entidad', 'sexo', 'nombre_entidad']]
+    is_entidad = df['nombre_entidad'] == selected_entidad
+    if selected_entidad!='Nacional':
+        df = df[is_entidad]
+
+    # Grouping and calculating proportions
+    df_counts = (
+        df.groupby(['year'])['nivelaprob']
+        .value_counts(normalize=True)
+        .rename('percentage')
+        .reset_index()
+    )
+
+    # Pivoting the data to get a better structure for plotting
+    df_pivot = df_counts.pivot(index='year', columns='nivelaprob', values='percentage').fillna(0)
+
+    # Plotting the bar graph
+    fig, ax = plt.subplots(figsize=(10, 6))
+    df_pivot.plot(kind='bar', stacked=True, ax=ax, colormap='viridis')
+
+    
+    fig.patch.set_facecolor(background_color)  # Figure background
+    ax.set_facecolor(background_color)
+
+    # Customizing the plot
+    ax.set_title('Proporciones de Grado Aprobado por Año',color=text_color)
+    ax.set_xlabel('Año',color=text_color)
+    ax.set_ylabel('Porcentaje',color=text_color)
+    ax.tick_params(colors=text_color)
+    ax.legend(title='Gradoaprob', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.tight_layout()
+
+    # Display the plot in the Streamlit app
+    st.pyplot(fig)
+
+def plot_proportion_by_generation(population, year,selected_entidad,text_color,background_color):
+    # Grouping the data
+    df=population
+    is_entidad = df['nombre_entidad'] == selected_entidad
+    if selected_entidad!='Nacional':
+        df= df[is_entidad]
+
+    data = df.groupby(['year', 'nivel_nombre'], as_index=False)['generacion'].value_counts(normalize=True)
+    data = data[data['year'] == year]
+
+    
+
+    # Pivoting the data
+    pivot_df = data.pivot(index='generacion', columns='nivel_nombre', values='proportion')
+
+    # Plotting the data
+    fig, ax = plt.subplots(figsize=(12, 6))
+    pivot_df.plot(kind='bar', ax=ax, colormap='viridis', width=0.8)
+
+    
+    fig.patch.set_facecolor(background_color)  # Figure background
+    ax.set_facecolor(background_color)
 
     # Formatting the plot
-    plt.title("Generational Income Over Years")
-    plt.ylabel("Trimester Income (MXN)")
-    plt.xlabel("Year")
-    plt.legend(title="Generation", bbox_to_anchor=(1.05, 1), loc='upper left')
+    ax.set_title(f"Tazas de proporción de grado aprobado Nacional {year}",color=text_color)
+    ax.set_ylabel("Taza ",color=text_color)
+    ax.set_xlabel("Generacion",color=text_color)
+    ax.tick_params(colors=text_color)
+    ax.legend(title="Generation", bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
-    st.pyplot(plt)
+
+    # Display the plot in the Streamlit dashboard
+    st.pyplot(fig)
+
+def plot_income_by_grade_and_year(income, population,selected_entidad,text_color,background_color):
+    # Merging the dataframes
+    df = pd.merge(income, population, on=['folioviv', 'foliohog', 'numren', 'year', 'entidad'], how='inner')
+
+    # Selecting relevant columns
+    df = df[['folioviv', 'foliohog', 'sexo', 'numren', 'edad', 'generacion',
+             'entidad','nombre_entidad', 'ing_tri', 'nivel_nombre', 'year']]
+
+    is_entidad = df['nombre_entidad'] == selected_entidad
+    if selected_entidad!='Nacional':
+        df = df[is_entidad]
+                
+    # Calculating total income per group
+    df['ing_tri_total'] = df.groupby(['folioviv', 'foliohog', 'numren', 'year'])['ing_tri'].transform('sum')
+
+    # Grouping by year and level name, calculating average income
+    data = df.groupby(['year', 'nivel_nombre'], as_index=False)['ing_tri_total'].mean()
+    
+    # Calculating monthly income
+    data['ing_mens'] = data['ing_tri_total'] / 3
+
+    # Pivoting the data for the plot
+    pivot_df = data.pivot(index='year', columns='nivel_nombre', values='ing_mens')
+
+    # Plotting the bar graph
+    fig, ax = plt.subplots(figsize=(12, 6))
+    pivot_df.plot(kind='bar', ax=ax, colormap='viridis', width=0.8)
+
+    
+    fig.patch.set_facecolor(background_color)  # Figure background
+    ax.set_facecolor(background_color)
+
+    # Formatting the plot
+    ax.set_title("Grade Income Over Years",color=text_color)
+    ax.set_ylabel("Trimester Income (MXN)",color=text_color)
+    ax.set_xlabel("Year",color=text_color)
+    ax.tick_params(colors=text_color)
+    ax.legend(title="Grade", bbox_to_anchor=(1.05, 1), loc='upper left')
+    
+
+    plt.tight_layout()
+
+    # Display the plot in the Streamlit app
+    st.pyplot(fig)
+
+
+
+
+# Display map
+def display_map():
+    
+    deck = pdk.Deck(
+        layers=[
+            pdk.Layer(
+                "GeoJsonLayer",
+                data=gdf.__geo_interface__,  # Convert GeoDataFrame to GeoJSON
+                get_fill_color="[255, 180, 180]",
+                get_line_color="[200, 0, 0]",
+                pickable=True,  # Enables interaction
+                auto_highlight=True,
+            )
+        ],
+        initial_view_state=pdk.ViewState(
+            latitude=gdf['lat'].mean(),
+            longitude=gdf['lon'].mean(),
+            zoom=4,
+        ),
+        tooltip={"html": "<b>Estado:</b> {NOMGEO}"},
+    )
+
+    # Render the map in Streamlit
+    map_data = st.pydeck_chart(deck)
+    return map_data
+
 
 # Heatmap
 def make_heatmap(dataframe, columns):
@@ -145,7 +355,7 @@ def make_heatmap(dataframe, columns):
     gdf = gdf.to_crs(epsg=4326)
 
     # Create Folium map
-    m = folium.Map(location=[gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()], zoom_start=6)
+    m = folium.Map(location=[gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()], zoom_start=4)
     
     # Add choropleth layer
     folium.Choropleth(
@@ -263,14 +473,26 @@ with col[0]:
  
 
 with col[1]:
-    st.markdown('#### Total Population')
-    population_columns= ['folioviv', 'foliohog', 'numren', 'entidad','sexo', 'edad','generacion','alfabetism', 'asis_esc', 'nivel','grado', 'tipoesc','nivelaprob', 'gradoaprob']
-    selected_entity=1
-    #make_barplot(df_selected_year,selected_entity)
+    st.markdown('#### Selecciona Estado')
+    map_data = display_map()
+    col1, col2 = st.columns(2)
+
+    # Nacional
+    with col1:
     
-    heatmap = make_heatmap(df_selected_year,population_columns)
-    st.components.v1.html(heatmap._repr_html_(), height=600)
-    
+        plot_proportion_by_generation(population, selected_year,'Nacional',text_color,background_color)
+        plot_income_by_grade_and_year(income, population,'Nacional',text_color,background_color)
+        plot_proportion_by_year(population,'Nacional',text_color,background_color)
+        plot_literacy_proportion_over_time(population,'Nacional',text_color,background_color)
+
+    # Estatal
+    with col2:
+
+        plot_proportion_by_generation(population, selected_year,selected_entidad,text_color,background_color)
+        plot_income_by_grade_and_year(income, population,selected_entidad,text_color,background_color)
+        plot_proportion_by_year(population,selected_entidad,text_color,background_color)
+        plot_literacy_proportion_over_time(population,selected_entidad,text_color,background_color)
+        
 
 with col[2]:
     st.markdown('#### Top States')
